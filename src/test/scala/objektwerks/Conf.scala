d98530ecaeefb0bfb7acd833bfe1bb4b968d8ed6
objektwerks <objektwerks@runbox.com>
2023-09-13 11:37:12 -0400

    reformatted conf

--- src/test/scala/objektwerks/Conf.scala
+++ src/test/scala/objektwerks/Conf.scala
@@ -1,9 +1,8 @@
 package objektwerks
 
-object Conf {
+object Conf:
   val minioUrl = "http://127.0.0.1:9000"
   val accessKey = sys.env.getOrElse(key = "MINIO_ACCESS_KEY", default = "minio access key unavailable")
   val secretKey = sys.env.getOrElse(key = "MINIO_SECRET_KEY", default = "minio secret key unavailable")
   val objectName = "gplv3"
   val pathToTextFile = "./gplv3.txt"
-}
